62baadc085445af24864b37a1eb97e1d394c695d
USMCFighter94 <verlander94@gmail.com>
2019-03-05 18:15:18 -0500

    Add GSON to ktor

---
+++ backend/src/main/kotlin/backend/Figure.kt
@@ -0,0 +1,3 @@
+package backend
+
+data class Figure(val id: Int, val name: String, val num: Int, val series: String, val year: Int)
--- backend/src/main/kotlin/backend/Server.kt
+++ backend/src/main/kotlin/backend/Server.kt
@@ -1,7 +1,11 @@
 package backend
 
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
 import io.ktor.http.ContentType
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
@@ -15,12 +19,20 @@ fun main() {
     }"""
 
     embeddedServer(Netty, 8080) {
+        install(ContentNegotiation) {
+            gson {
+                setPrettyPrinting()
+            }
+        }
         routing {
             get("/") {
                 call.respondText("Hello from Kotlin backend", ContentType.Text.Html)
             }
             get("kolin") {
                 call.respondText(kolinResponse, ContentType.Application.Json)
+            }
+            get("figure") {
+                call.respond(Figure(1, "Finn (Jakku)", 1, "The Black Series - The Force Awakens", 2015))
             }
         }
     }.start(true)
