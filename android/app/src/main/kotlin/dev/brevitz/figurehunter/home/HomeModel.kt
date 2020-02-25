e0edb17ad4fc0d28b41c6cddec770e141a00426e
USMCFighter94 <verlander94@gmail.com>
2020-02-24 21:24:40 -0500

    Try out view binding in Home's epoxy model

--- android/app/src/main/kotlin/dev/brevitz/figurehunter/home/HomeModel.kt
+++ android/app/src/main/kotlin/dev/brevitz/figurehunter/home/HomeModel.kt
@@ -1,23 +1,30 @@
 package dev.brevitz.figurehunter.home
 
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.airbnb.epoxy.EpoxyModelWithView
 import dev.brevitz.figurehunter.R
 import dev.brevitz.figurehunter.core.domain.home.Figure
 import dev.brevitz.figurehunter.core.ui.inflateAs
+import dev.brevitz.figurehunter.databinding.ItemHomeBinding
 
 data class HomeModel(private val figure: Figure) : EpoxyModelWithView<ConstraintLayout>() {
 
+    private var binding: ItemHomeBinding? = null
+
     override fun bind(view: ConstraintLayout) {
         super.bind(view)
-        with(view) {
-            findViewById<TextView>(R.id.homeItemName).text = figure.name
-            findViewById<TextView>(R.id.homeItemNumber).text = figure.num.toString()
-            findViewById<TextView>(R.id.homeItemSeries).text = figure.series
-            findViewById<TextView>(R.id.homeItemYear).text = figure.year.toString()
-        }
+        binding = ItemHomeBinding.bind(view)
+
+        binding?.homeItemName?.text = figure.name
+        binding?.homeItemNumber?.text = figure.num.toString()
+        binding?.homeItemSeries?.text = figure.series
+        binding?.homeItemYear?.text = figure.year.toString()
+    }
+
+    override fun unbind(view: ConstraintLayout) {
+        super.unbind(view)
+        binding = null
     }
 
     override fun buildView(parent: ViewGroup): ConstraintLayout = parent.inflateAs(R.layout.item_home)
